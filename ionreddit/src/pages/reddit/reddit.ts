d0ba6f1229d2cbb3a13e5c49cd2152f350bdd873
Sebastian Guerrero Galvan (seg930) <seg930@mail.usask.ca>
2018-03-31 12:41:59 -0600

    added files'

---
+++ ionreddit/src/pages/reddit/reddit.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'reddit',
+  templateUrl: 'reddit.html'
+})
+export class RedditPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
