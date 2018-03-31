78de2bffe7b6b2b91a36b0583018a4e3aef4c8e6
Mitch_Benkic <mib352@mail.usask.ca>
2018-03-31 12:53:11 -0600

    Stuff to do

--- ionreddit/src/pages/tabs/tabs.ts
+++ ionreddit/src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { SettingsPage } from '../settings/settings';
+import { RedditPage } from '../reddit/reddit';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = RedditPage;
+  tab2Root = SettingsPage;
+  tab3Root = AboutPage;
 
   constructor() {
 
